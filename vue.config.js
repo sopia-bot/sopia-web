1f71c6fe3a89e5823c2df82890f46a7c4ae85e65
TreeSome <youn@tree-some.dev>
2020-03-12 22:40:55 +0900

    prerender 추가

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,39 @@
 const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const plugins = [];
+
+if ( process.argv[2] === "build" ) {
+	plugins.push(new PrerenderSpaPlugin({
+		staticDir: path.join(__dirname, 'dist'),
+		indexPath: path.join(__dirname, 'dist', 'index.html'),
+		routes: [
+			"/",
+			"/docs/quick-start/install/",
+			"/docs/quick-start/manager/",
+			"/docs/quick-start/load-setting/",
+			"/docs/quick-start/spoor-chat/",
+			"/docs/menus/home/",
+			"/docs/menus/code/",
+			"/docs/menus/spoor-chat/",
+			"/docs/menus/setting/",
+			"/docs/menus/bundle/",
+			"/docs/bundle/onoff/",
+			"/docs/bundle/loop/",
+			"/docs/bundle/signature/",
+			"/docs/bundle/song-request/",
+			"/docs/bundle/vote/",
+			"/docs/api/start/",
+			"/docs/api/sopia/",
+			"/docs/api/var/",
+
+		],
+	}));
+}
 
 module.exports = {
 	configureWebpack: {
+		plugins,
 		resolve: {
 			alias: {
 				'@': path.join(__dirname, 'src/'),
